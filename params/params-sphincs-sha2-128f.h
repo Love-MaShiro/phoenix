4b70ab83fd224ba9d572119c02a2a0d83c8e0036
yzc <1491328783@qq.com>
2026-04-24 10:31:23 +0800

    修改了参数

--- params/params-sphincs-sha2-128f.h
+++ params/params-sphincs-sha2-128f.h
@@ -15,8 +15,6 @@
 #define SPX_TFORS_LOG_K_PRIME 5
 #define SPX_TFORS_K 17
 #define SPX_TFORS_HEIGHT (SPX_TFORS_LOG_K_PRIME + SPX_TFORS_A)
-#define SPX_TFORS_T (1 << SPX_TFORS_A)
-
 /* Winternitz parameter, */
 #define SPX_WOTS_W1 16
 #define SPX_WOTS_W2 32
@@ -44,8 +42,8 @@
 #error SPX_WOTS_W2 assumed 32 or 512
 #endif
 
-#define SPX_WOTS_W1_LEN 26
-#define SPX_WOTS_W2_LEN 4
+#define SPX_WOTS_W1_LEN 22
+#define SPX_WOTS_W2_LEN 8
 #define SPX_WOTS_LEN1 (SPX_WOTS_W1_LEN + SPX_WOTS_W2_LEN)
 
 /* SPX_WOTS_LEN2 is fixed */
@@ -59,7 +57,7 @@
 #define SPX_TREE_HEIGHT (SPX_FULL_HEIGHT / SPX_D)
 
 #if SPX_TREE_HEIGHT * SPX_D != SPX_FULL_HEIGHT
-    #error SPX_D should always divide SPX_FULL_HEIGHT
+#error SPX_D should always divide SPX_FULL_HEIGHT
 #endif
 
 /* TFORS parameters. */
@@ -73,7 +71,7 @@
 #define COUNTER_SIZE 4
 
 /* Search and checksum range definition */
-#define SPX_WOTS_ZERO_LAST_BITS 4
+#define SPX_WOTS_ZERO_LAST_BITS 0
 #define WOTS_ZERO_BITS SPX_WOTS_ZERO_LAST_BITS
 
 /* --- WOTS+C Automatic Parameter Calculation --- */
@@ -81,23 +79,41 @@
 #define SPX_WOTS_CHECKSUM_W 16
 #define SPX_WOTS_CHECKSUM_LOGW 4
 
-/* 
+/*
  * Calculate the expected average sum (E) of the message chains.
+ * Formula: E = Sum of (Length_i * (W_i - 1) / 2)
+ * We multiply by Length first to avoid floating point issues in macros.
  */
-#define WOTS_EXPECTED_SUM ( \
-    ((SPX_WOTS_W1_LEN * (SPX_WOTS_W1 - 1)) + \
-     (SPX_WOTS_W2_LEN * (SPX_WOTS_W2 - 1))) / 2 \
-)
+#define WOTS_EXPECTED_SUM (                   \
+    ((SPX_WOTS_W1_LEN * (SPX_WOTS_W1 - 1)) +  \
+     (SPX_WOTS_W2_LEN * (SPX_WOTS_W2 - 1))) / \
+    2)
 
+/*
+ * WOTS_SUM_BASE: The starting point of the acceptable sum range.
+ * We center the range around the expected average sum.
+ */
 #define WOTS_SUM_BASE (WOTS_EXPECTED_SUM - (SPX_WOTS_CHECKSUM_W / 2))
-#define WOTS_SUM_RANGE ((SPX_WOTS_CHECKSUM_W)-1)
+
+/*
+ * WOTS_SUM_RANGE: The total number of sum values covered by one checksum chain.
+ * For a chain with Winternitz parameter W, it can represent W distinct values (0 to W-1).
+ */
+#define WOTS_SUM_RANGE ((SPX_WOTS_CHECKSUM_W) - 1)
+
+/*
+ * Note: The valid sum interval is [WOTS_SUM_BASE, WOTS_SUM_BASE + WOTS_SUM_RANGE)
+ * For n=128, W1=16, W2=32, W1_LEN=22, W2_LEN=8:
+ * E = (22*15 + 8*31) / 2 = (330 + 248) / 2 = 289
+ * BASE = 289 - 8 = 281
+ * RANGE = 15
+ */
 
 /* Resulting SPX sizes. */
 #define SPX_BYTES (SPX_N + COUNTER_SIZE + SPX_TFORS_SIG_MAX + SPX_D * (SPX_WOTS_BYTES + COUNTER_SIZE) + \
                    SPX_FULL_HEIGHT * SPX_N)
 #define SPX_PK_BYTES (2 * SPX_N)
 #define SPX_SK_BYTES (2 * SPX_N + SPX_PK_BYTES)
-
 #include "../sha2_offsets.h"
 
 #endif
--- params/params-sphincs-sha2-192f.h
+++ params/params-sphincs-sha2-192f.h
@@ -6,60 +6,48 @@
 /* Hash output length in bytes. */
 #define SPX_N 24
 /* Height of the hypertree. */
-#define SPX_FULL_HEIGHT 64
+#define SPX_FULL_HEIGHT 65
 /* Number of subtree layer. */
-#define SPX_D 16
+#define SPX_D 13
 /* TFORS tree dimensions. */
 #define SPX_TFORS_A 9
 #define SPX_TFORS_K_PRIME 32
 #define SPX_TFORS_LOG_K_PRIME 5
-#define SPX_TFORS_K 30
+#define SPX_TFORS_K 27
 #define SPX_TFORS_HEIGHT (SPX_TFORS_LOG_K_PRIME + SPX_TFORS_A)
 /* Winternitz parameter, */
-#define SPX_WOTS_W 16
+#define SPX_WOTS_W1 16
+#define SPX_WOTS_W2 32
 
 /* The hash function is defined by linking a different hash.c file, as opposed
    to setting a #define constant. */
-
-/* This is a SHA2-based parameter set, hence whether we use SHA-256
- * exclusively or we use both SHA-256 and SHA-512 is controlled by
- * the following #define */
-#define SPX_SHA512 1  /* Use SHA-512 for H and T_l, l >= 2 */
 
 /* For clarity */
 #define SPX_ADDR_BYTES 32
 
 /* WOTS parameters. */
-#if SPX_WOTS_W == 256
-    #define SPX_WOTS_LOGW 8
-#elif SPX_WOTS_W == 16
-    #define SPX_WOTS_LOGW 4
+#if SPX_WOTS_W1 == 256
+#define SPX_WOTS_LOGW1 8
+#elif SPX_WOTS_W1 == 16
+#define SPX_WOTS_LOGW1 4
 #else
-    #error SPX_WOTS_W assumed 16 or 256
+#error SPX_WOTS_W1 assumed 16 or 256
 #endif
 
-#define SPX_WOTS_LEN1 (8 * SPX_N / SPX_WOTS_LOGW)
-
-/* SPX_WOTS_LEN2 is floor(log(len_1 * (w - 1)) / log(w)) + 1; we precompute */
-#if SPX_WOTS_W == 256
-    #if SPX_N <= 1
-        #define SPX_WOTS_LEN2 1
-    #elif SPX_N <= 256
-        #define SPX_WOTS_LEN2 2
-    #else
-        #error Did not precompute SPX_WOTS_LEN2 for n outside {2, .., 256}
-    #endif
-#elif SPX_WOTS_W == 16
-    #if SPX_N <= 8
-        #define SPX_WOTS_LEN2 2
-    #elif SPX_N <= 136
-        #define SPX_WOTS_LEN2 3
-    #elif SPX_N <= 256
-        #define SPX_WOTS_LEN2 4
-    #else
-        #error Did not precompute SPX_WOTS_LEN2 for n outside {2, .., 256}
-    #endif
+#if SPX_WOTS_W2 == 32
+#define SPX_WOTS_LOGW2 5
+#elif SPX_WOTS_W2 == 512
+#define SPX_WOTS_LOGW2 9
+#else
+#error SPX_WOTS_W2 assumed 32 or 512
 #endif
+
+#define SPX_WOTS_W1_LEN 48
+#define SPX_WOTS_W2_LEN 0
+#define SPX_WOTS_LEN1 (SPX_WOTS_W1_LEN + SPX_WOTS_W2_LEN)
+
+/* SPX_WOTS_LEN2 is fixed */
+#define SPX_WOTS_LEN2 1
 
 #define SPX_WOTS_LEN (SPX_WOTS_LEN1 + SPX_WOTS_LEN2)
 #define SPX_WOTS_BYTES (SPX_WOTS_LEN * SPX_N)
@@ -69,7 +57,7 @@
 #define SPX_TREE_HEIGHT (SPX_FULL_HEIGHT / SPX_D)
 
 #if SPX_TREE_HEIGHT * SPX_D != SPX_FULL_HEIGHT
-    #error SPX_D should always divide SPX_FULL_HEIGHT
+#error SPX_D should always divide SPX_FULL_HEIGHT
 #endif
 
 /* TFORS parameters. */
@@ -79,8 +67,50 @@
 #define SPX_TFORS_SIG_MAX (SPX_TFORS_BYTES * 7) / 10
 #define SPX_TFORS_PK_BYTES SPX_N
 
+/* custom upgrade parameter definitions */
+#define COUNTER_SIZE 4
+
+/* Search and checksum range definition */
+#define SPX_WOTS_ZERO_LAST_BITS 0
+#define WOTS_ZERO_BITS SPX_WOTS_ZERO_LAST_BITS
+
+/* --- WOTS+C Automatic Parameter Calculation --- */
+/* Winternitz parameter for the single checksum chain */
+#define SPX_WOTS_CHECKSUM_W 16
+#define SPX_WOTS_CHECKSUM_LOGW 4
+
+/*
+ * Calculate the expected average sum (E) of the message chains.
+ * Formula: E = Sum of (Length_i * (W_i - 1) / 2)
+ * We multiply by Length first to avoid floating point issues in macros.
+ */
+#define WOTS_EXPECTED_SUM (                   \
+    ((SPX_WOTS_W1_LEN * (SPX_WOTS_W1 - 1)) +  \
+     (SPX_WOTS_W2_LEN * (SPX_WOTS_W2 - 1))) / \
+    2)
+
+/*
+ * WOTS_SUM_BASE: The starting point of the acceptable sum range.
+ * We center the range around the expected average sum.
+ */
+#define WOTS_SUM_BASE (WOTS_EXPECTED_SUM - (SPX_WOTS_CHECKSUM_W / 2))
+
+/*
+ * WOTS_SUM_RANGE: The total number of sum values covered by one checksum chain.
+ * For a chain with Winternitz parameter W, it can represent W distinct values (0 to W-1).
+ */
+#define WOTS_SUM_RANGE ((SPX_WOTS_CHECKSUM_W) - 1)
+
+/*
+ * Note: The valid sum interval is [WOTS_SUM_BASE, WOTS_SUM_BASE + WOTS_SUM_RANGE)
+ * For n=128, W1=16, W2=32, W1_LEN=22, W2_LEN=8:
+ * E = (22*15 + 8*31) / 2 = (330 + 248) / 2 = 289
+ * BASE = 289 - 8 = 281
+ * RANGE = 15
+ */
+
 /* Resulting SPX sizes. */
-#define SPX_BYTES (SPX_N + 4 + SPX_TFORS_SIG_MAX + SPX_D * SPX_WOTS_BYTES +\
+#define SPX_BYTES (SPX_N + COUNTER_SIZE + SPX_TFORS_SIG_MAX + SPX_D * (SPX_WOTS_BYTES + COUNTER_SIZE) + \
                    SPX_FULL_HEIGHT * SPX_N)
 #define SPX_PK_BYTES (2 * SPX_N)
 #define SPX_SK_BYTES (2 * SPX_N + SPX_PK_BYTES)
